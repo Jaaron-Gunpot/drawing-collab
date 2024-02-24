b0d00e02e1b9744b1fa7f92f5a32d1a6f3d17c59
Jaaron-Gunpot <jsw6194@rit.edu>
2024-02-24 18:56:28 -0500

    I realize i have done nothing yet

--- client/js/canvas.js
+++ client/js/canvas.js
@@ -1,5 +1,13 @@
+//objects that holds the canvas object to be detected when it changes
+let oldCanvas;
+let newCanvas;
+
 function setup() {
-    createCanvas(400, 400);
+    //since this uses pointer, it automatically updates to the newest canvas
+    oldCanvas = createCanvas(400, 400);
+
+    //creates a deep clone of the canvas to compare to
+    newCanvas = Object.assign({},oldCanvas);
 }
 
 function draw() {
@@ -7,3 +15,12 @@ function draw() {
     fill('red');
     circle(mouseX, mouseY, 50);
 }
+
+const socket = io();
+socket.on('connect', () => {
+    console.log(socket.id);
+});
+const canvasChanged = () =>{
+    socket.emit('canvas-changed', "canvas changed!");
+};
+setInterval(canvasChanged,5000);
--- src/sockets.js
+++ src/sockets.js
@@ -2,4 +2,7 @@ const { io } = require('./server.js');
 
 io.on('connection', (socket) => {
   console.log(`connected socket:${socket.id}`);
+  socket.on('canvas-changed',(e)=>{
+    console.log(e);
+  });
 });
