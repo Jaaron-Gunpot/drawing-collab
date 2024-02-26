735985bbebb985c11068ffaf6c2cbcfe8d0910e3
Jaaron <jsw6194@rit.edu>
2024-02-25 21:19:16 -0500

    I'm not sure if it works yet

--- src/canvas.js
+++ src/canvas.js
@@ -1,14 +1,19 @@
+const socket = require('./sockets.js').canvasChanged;
 //empry object to hold the most current canvas object
-const currentCanvas = {};
+let currentCanvas = {};
+
 
 //function to update and emit the current canvas object
+//I don't know how to parse the image data into something usable
 const updateCanvas = (request, response, body) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.writeHead(204, { 'Content-Type': 'application/json' });
+    currentCanvas = body.data;
+    socket(currentCanvas);
     response.write(JSON.stringify(currentCanvas));
     response.end();
     console.log(body);
 };
 
 module.exports = { 
-        updateCanvas
+    updateCanvas
 };
--- src/sockets.js
+++ src/sockets.js
@@ -1,8 +1,18 @@
 const { io } = require('./server.js');
 
+//dummy function to be assigned in socket so it can be exported
+let canvasChanged;
+
 io.on('connection', (socket) => {
   console.log(`connected socket:${socket.id}`);
   socket.on('canvas-changed',(e)=>{
     console.log(e);
   });
+  canvasChanged = (e) => {
+    socket.emit('canvas-changed', e);
+  };
 });
+
+module.exports = {
+  canvasChanged,
+};
