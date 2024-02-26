9675c38667c34e802331eb2e54b44004da41df66
Jaaron <jsw6194@rit.edu>
2024-02-25 22:33:11 -0500

    fixed socket error

--- src/sockets.js
+++ src/sockets.js
@@ -9,21 +9,21 @@ let io;
 // setup the io object using a passed in server from server.js
 const setupSockets = (server) => {
   io = socketio(server);
+  // I want to export socket so i can emit events from other files but this is the entry file - rectified
+  io.on('connection', (socket) => {
+    console.log(`connected socket:${socket.id}`);
+    socket.on('canvas-changed', (e) => {
+      //console.log(e);
+    });
+    // tells connected sockets that the canvas has changed so they can update accordingly
+    // at this point I realized sockets are just an implementation of the observer pattern
+    canvasChanged = (e) => {
+      socket.emit('new-canvas', e);
+    };
+  });
   return io;
 };
 
-// I want to export socket so i can emit events from other files but this is the entry file
-io.on('connection', (socket) => {
-  console.log(`connected socket:${socket.id}`);
-  socket.on('canvas-changed', (e) => {
-    console.log(e);
-  });
-  // tells connected sockets that the canvas has changed so they can update accordingly
-  // at this point I realized sockets are just an implementation of the observer pattern
-  canvasChanged = (e) => {
-    socket.emit('new-canvas', e);
-  };
-});
 
 module.exports = {
   setupSockets,
