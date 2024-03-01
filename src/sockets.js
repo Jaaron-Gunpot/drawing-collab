9dcbab8a66f200a801476250860f5abd40886cd8
Jaaron-Gunpot <jsw6194@rit.edu>
2024-03-01 10:44:13 -0500

    I think its there soon

--- src/sockets.js
+++ src/sockets.js
@@ -9,6 +9,15 @@ const canvasChanged = (e) => {
   io.to('writing-room').emit('new-canvas', e);
 };
 
+//I got this from a demo but I am not sure I need it yet
+const handleRoomChange = (socket, roomName) => {
+  socket.rooms.forEach(room => {
+      if(room == socket.id) return; 
+      socket.leave(room);
+  });
+  socket.join(roomName);
+};
+
 // setup the io object using a passed in server from server.js
 const setupSockets = (server) => {
   io = socketio(server);
@@ -16,15 +25,19 @@ const setupSockets = (server) => {
   // I want to export socket so i can emit events from other files but this is the entry file-fixed
   io.on('connection', (socket) => {
     socket.join('writing-room');
+
     console.log(`connected socket:${socket.id}`);
     // should the server do something if the canvas hanges soket related?
     // the client already sends a post request when that happens so probably not
     socket.on('canvas-changed', () => {
       canvasChanged();
     });
+    socket.on('room change', (room) => handleRoomChange(socket, room));
   });
   return io;
 };
+
+
 
 module.exports = {
   setupSockets,
