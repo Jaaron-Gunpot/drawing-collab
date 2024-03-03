f8feaffd838b12d61ad9779b62f8bc4ade5c4254
Jaaron <jsw6194@rit.edu>
2024-03-03 00:46:01 -0500

    setup query parameters

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,1 +1,26 @@
 const { rooms } = require('./sockets.js');
+
+//If there is a room that already exists, give it to them
+//If there is no room, create one
+const createRoom = (request, response, body) => {
+    roomName = body.data.room;
+    //if they did'nt give a room name
+    if (roomName === undefined) {
+        response.writeHead(400, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify({ message: 'room name required' }));
+        response.end();
+    }//if the room name alreday exists(shouldn't happen but just to be safe)
+    else if (rooms.includes(roomName)) {
+        //I don't know what status code to use
+        response.writeHead(409, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify({ message: 'room already exists' }));
+        response.end();
+    } else {//Create the room
+        rooms.push(roomName);
+        response.writeHead(201, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify({ message: 'room created' }));
+        response.end();
+    }
+};
+
+module.exports.createRoom = createRoom;
